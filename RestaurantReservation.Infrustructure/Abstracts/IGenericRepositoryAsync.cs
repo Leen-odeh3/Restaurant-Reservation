571d1c254f2c1bbc2e7fdd14ffc0a6f3a5cf9a34
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-15 00:10:44 +0300

    add IGeneric Repository

---
+++ RestaurantReservation.Infrustructure/Abstracts/IGenericRepositoryAsync.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace RestaurantReservation.Infrustructure.Abstracts;
+public interface IGenericRepositoryAsync<T> where T : class
+{
+    Task DeleteRangeAsync(ICollection<T> entities);
+    Task<T> GetByIdAsync(int id);
+    Task SaveChangesAsync();
+    IDbContextTransaction BeginTransaction();
+    void Commit();
+    void RollBack();
+    IQueryable<T> GetTableNoTracking();
+    IQueryable<T> GetTableAsTracking();
+    Task<T> AddAsync(T entity);
+    Task AddRangeAsync(ICollection<T> entities);
+    Task UpdateAsync(T entity);
+    Task UpdateRangeAsync(ICollection<T> entities);
+    Task DeleteAsync(T entity);
+}
