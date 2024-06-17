b380593996448f4d49f81428b647b8c6f6b16539
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-18 02:10:04 +0300

    Mapping Employee & Response

--- RestaurantReservation.Core/RestaurantReservation.Core/Mapping/Employees/EmployeeProfile.cs
+++ RestaurantReservation.Core/RestaurantReservation.Core/Mapping/Employees/EmployeeProfile.cs
@@ -4,14 +4,12 @@ using RestaurantReservation.Domain.Entities;
 
 namespace RestaurantReservation.Core.Mapping.Employees;
 
-public class EmployeeProfile : Profile
+public partial class EmployeeProfile : Profile
 {
     public EmployeeProfile()
     {
-        CreateMap<Employee, GetEmployeeListResponse>()
-            .ForMember(dest => dest.EmployeeID, opt => opt.MapFrom(src => src.EmployeeID))
-            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
-            .ForMember(dest => dest.Position, opt => opt.MapFrom(src => src.Position.ToString()))
-            .ForMember(dest => dest.RestaurantName, opt => opt.MapFrom(src => src.Restaurant.Name));
+        AddEmployeeMapping();
+        EditEmployeeMapping();
+        GetEmployeeMapping();
     }
 }
---
+++ RestaurantReservation.Core/RestaurantReservation.Core/Mapping/Employees/QueryMapping/GetEmployeeListMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestaurantReservation.Core.Features.Emoloyees.Queries.Results;
+using RestaurantReservation.Core.Features.MenuItems.Queries.Results;
+using RestaurantReservation.Domain.Entities;
+namespace RestaurantReservation.Core.Mapping.Employees;
+public partial class EmployeeProfile
+{
+    public void GetEmployeeMapping()
+    {
+        CreateMap<Employee, GetEmployeeListResponse>()
+           .ForMember(dest => dest.EmployeeID, opt => opt.MapFrom(src => src.EmployeeID))
+            .ForMember(dest => dest.RestaurantName, opt => opt.MapFrom(src => src.Restaurant.Name));
+    }
+}
